dd22505a029bd7948c0aba43044117bf6b5cac76
theblackman2 <paskasonga@gmail.com>
2022-10-06 11:23:48 +0100

    Initialized project with hello word message

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("Hello from the backend");
+});
+
+app.all("*", (request, response) => {
+  response.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}`);
+});
